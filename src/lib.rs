e2354332736cdf8bff798ad203a03036eab4f194
Zedjones <dojoman19@gmail.com>
2020-02-29 23:05:27 -0500

    Need to figure out how to do async calls for tests

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,14 @@ mod client;
 
 #[cfg(test)]
 mod tests {
+    use crate::client::Client;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn search_client() {
+        let client = Client::new();
+        //client.search_word(String::from("iie")).await;
     }
 }
