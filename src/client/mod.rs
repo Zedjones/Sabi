17513af175d6eb24a5d13975a95397a60d4eb74f
Zedjones <dojoman19@gmail.com>
2020-02-29 23:11:14 -0500

    Fixed some formatting

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,21 +1,21 @@
 extern crate serde;
 extern crate reqwest;
 
-    use crate::models::*;
+use crate::models::*;
 
-    pub struct Client {
-        client: reqwest::Client
+pub struct Client {
+    client: reqwest::Client
+}
+
+impl Client {
+    pub fn new() -> Client {
+        Client { client: reqwest::Client::new() }
     }
 
-    impl Client {
-        pub fn new() -> Client {
-            Client { client: reqwest::Client::new() }
-        }
+    pub async fn search_word(&self, word: String) -> Result<Word, reqwest::Error>{
+        let full_url = format!("https://jisho.org/api/v1/search/words?keyword={}", word);
 
-        pub async fn search_word(&self, word: String) -> Result<Word, reqwest::Error>{
-            let full_url = format!("https://jisho.org/api/v1/search/words?keyword={}", word);
-    
-            let resp: Word = self.client.get(&full_url).send().await?.json().await?;
-            Ok(resp)
-        }
+        let resp: Word = self.client.get(&full_url).send().await?.json().await?;
+        Ok(resp)
     }
+}
