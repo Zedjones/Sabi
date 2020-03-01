be9c8dc4e2f4ddf34f268e92d82366bd3ad41e79
Zedjones <dojoman19@gmail.com>
2020-02-29 19:30:26 -0500

    Added serde renames for certain fields

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -6,6 +6,7 @@ pub mod models {
 
     #[derive(Serialize, Deserialize)]
     pub struct EnglishDefinition {
+        #[serde(rename = "english_definitions")]
         definitions: Vec<String>,
         parts_of_speech: Vec<String>,
         tags: Vec<String>,
@@ -19,9 +20,11 @@ pub mod models {
 
     #[derive(Serialize, Deserialize)]
     pub struct Word {
+        #[serde(rename = "senses")]
         english_definitions: Vec<EnglishDefinition>,
         is_common: bool,
         tags: Vec<String>,
+        #[serde(rename = "japanese")]
         japanese_words: Vec<JapaneseWord>,
     }
 }
