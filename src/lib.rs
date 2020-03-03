b3e4c23443361bb6e14eed69faddec12e58141a8
Zedjones <dojoman19@gmail.com>
2020-03-02 21:52:59 -0500

    Cleaned things up a bit

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,9 @@ pub use errors::{Result, SabiError};
 
 #[cfg(test)]
 mod tests {
-    use crate::client::Client;
+    use crate::Client;
+    use crate::Result;
+    use crate::Word;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
@@ -16,8 +18,7 @@ mod tests {
     #[tokio::test]
     async fn search_client() {
         let client = Client::new();
-        let res: crate::errors::Result<Vec<crate::models::Word>>;
-        res = client.search_english_word(String::from("computer")).await;
+        let res: Result<Vec<Word>> = client.search_english_word(String::from("computer")).await;
         match res {
             Ok(words) => {
                 println!("{:?}", words);
