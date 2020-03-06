7f9a92a10b69062c5120233e14c4e00d459e4d53
Zedjones <dojoman19@gmail.com>
2020-03-05 20:53:05 -0500

    Implemented test for basic word

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,8 @@ pub use errors::{Result, SabiError};
 
 #[cfg(test)]
 mod tests {
-    use crate::{Client, errors::Result};
+    use crate::{Client, errors::Result, models::Word};
+    use std::{fs::read_to_string, path::PathBuf};
 
     use mockito::mock;
     fn create_ok_mock(path: &str, json: &str) -> mockito::Mock {
@@ -33,8 +34,28 @@ mod tests {
         let term = "computer";
         let _m = create_fail_mock(term).create();
         let res = Client::new().search_japanese_word(term).await;
-        assert!(res.is_err());
         assert!(res.unwrap_err().to_string().contains("EOF while parsing a value"));
+        Ok(())
+    }
+    #[tokio::test]
+    async fn basic_word() -> Result<()> {
+        let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        path.push("src/test/test.json");
+        let term = "unten";
+        let json = read_to_string(path).unwrap();
+        let _m = create_ok_mock(term, &json).create();
+        let res: Vec<Word> = Client::new().search_japanese_word(term).await?;
+        let fst_item = res.first().expect("no first word");
+        assert!(fst_item
+                .english_definitions
+                .first()
+                .expect("no first element in english definitionss")
+                .definitions
+                .contains(&"operating".to_string()));
+        let fst_jp_word = fst_item.japanese_words.first().expect("no first japanese definition");
+        assert_eq!(fst_jp_word.word, Some("運転".to_string()));
+        assert_eq!(fst_jp_word.reading, Some("うんてん".to_string()));
+        assert_eq!(fst_item.is_common, Some(true));
         Ok(())
     }
 }
